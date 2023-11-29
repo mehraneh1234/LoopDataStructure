c0a6cd072a1ed72d4e1a804f2cee365a8e976946
mehraneh1234 <132540431+mehraneh1234@users.noreply.github.com>
2023-11-29 09:57:32 +0800

    version 1

---
+++ LoopDataStructure.py
@@ -0,0 +1,10 @@
+# Mehraneh 30062786
+# AT2.4
+# print out each element by using for loop
+
+# Define an array with 5 items
+carlist = ["Toyota Camry", "Ford Falcon", "Nissan Pulsar", "Ferrari F50", "BMW M5"]
+
+# Create a For Loop to print each item of the array on a new line.
+for x in carlist:
+    print(x);
